56db0fbebbf5d50a1c69aa3fdce828c23b46f8d4
Cole Duclos <cole.duclos@autodesk.com>
2021-12-19 11:58:18 -0700

    adding cognito and base for votes api

---
+++ lists-service/__tests__/votes.test.js
@@ -0,0 +1,154 @@
+const votes = require('../api/v1/votes');
+
+jest.setTimeout(1000);
+let mockId = '0';
+let mockUserId = '1';
+
+jest.mock("aws-sdk")
+
+const AWS = require('aws-sdk')
+
+test('Test getAll votes empty response w/ default limit', done => {
+  AWS.DynamoDB.DocumentClient.prototype.scan.mockImplementationOnce((_, callback) => callback(null, {
+    'Items' :[],
+    'Count' : 0
+  }));
+  const exampleEvent = {'queryStringParameters' : null}
+  const lambdaCallback = (err, data) => {
+    try {
+      expect(data.statusCode).toBe(200);
+      let responseBody = JSON.parse(data.body);
+      expect(responseBody.data.length).toBe(0);
+      expect(responseBody.count).toBe(0);
+      expect(responseBody.limit).toBe(100);
+      done();
+    } catch (error) {
+      done(error);
+    };
+  };
+  votes.getAll(exampleEvent, {}, lambdaCallback)
+});
+
+test('Test getAll votes non-empty response w/ default limit', done => {
+    AWS.DynamoDB.DocumentClient.prototype.scan.mockImplementationOnce((_, callback) => callback(null, {
+      'Items' :[{'voteId' : mockId, 'userId': mockUserId}],
+      'Count' : 1
+    }));
+    const exampleEvent = {'queryStringParameters' : null}
+    const lambdaCallback = (err, data) => {
+      try {
+        expect(data.statusCode).toBe(200);
+        let responseBody = JSON.parse(data.body);
+        expect(responseBody.data.length).toBe(1);
+        expect(responseBody.count).toBe(1);
+        expect(responseBody.limit).toBe(100);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.getAll(exampleEvent, {}, lambdaCallback)
+  });
+
+test('Test getAll votes non-empty response w/ explicit limit & pagination', done => {
+    const lastEvaluatedKey = { 'voteId': mockId };
+    AWS.DynamoDB.DocumentClient.prototype.scan.mockImplementationOnce((_, callback) => callback(null, {
+      'Items' :[{'vote' : mockId, 'userId': mockUserId}],
+      'Count' : 1,
+      'LastEvaluatedKey': lastEvaluatedKey
+    }));
+    const exampleEvent = {'queryStringParameters' : {'limit':1}}
+    const lambdaCallback = (err, data) => {
+      try {
+        expect(data.statusCode).toBe(200);
+        let responseBody = JSON.parse(data.body);
+        expect(responseBody.data.length).toBe(1);
+        expect(responseBody.count).toBe(1);
+        expect(responseBody.limit).toBe(1);
+        expect(responseBody.pagination.previousCursor).toBeNull();
+        let nextCursor = Buffer.from(responseBody.pagination.nextCursor, 'base64').toString('ascii')
+        expect(nextCursor).toBe(JSON.stringify(lastEvaluatedKey));
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.getAll(exampleEvent, {}, lambdaCallback)
+  });
+
+test('Test vote not found', done => {
+    AWS.DynamoDB.DocumentClient.prototype.get.mockImplementationOnce((_, callback) => callback(null, {}));
+    const exampleEvent = {'pathParameters' : {'voteId' : mockId}}
+    const lambdaCallback = (err, data) => {
+      try {
+        console.log(data)
+        expect(data.statusCode).toBe(404);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.getOne(exampleEvent, {}, lambdaCallback)
+  });
+
+test('Test get valid vote', done => {
+    AWS.DynamoDB.DocumentClient.prototype.get.mockImplementationOnce((_, callback) => callback(null, {'Item' : {'voteId' : mockId, 'userId': mockUserId}}));
+    const exampleEvent = {'pathParameters' : {'voteId' : mockId}}
+    const lambdaCallback = (err, data) => {
+      try {
+        let responseBody = JSON.parse(data.body);
+        expect(data.statusCode).toBe(200);
+        expect(responseBody.voteId).toBe(mockId);
+        expect(responseBody.userId).toBe(mockUserId);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.getOne(exampleEvent, {}, lambdaCallback)
+  });
+
+test('Test create valid vote', done => {
+    AWS.DynamoDB.DocumentClient.prototype.put.mockImplementationOnce((_, callback) => callback(null, {'Item': {'userId': mockUserId, 'score' : 1}}));
+    const exampleEvent = {'body' : '{\"userId\":\"'+mockUserId+'\",\"score\": 1}'}
+    const lambdaCallback = (err, data) => {
+      try {
+        let responseBody = JSON.parse(data.body);
+        expect(data.statusCode).toBe(200);
+        expect(responseBody.userId).toBe(mockUserId);
+        expect(responseBody.score).toBe(score);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.createOne(exampleEvent, {}, lambdaCallback)
+  });
+
+  test('Test delete valid vote', done => {
+    AWS.DynamoDB.DocumentClient.prototype.delete.mockImplementationOnce((_, callback) => callback(null, {'Attributes': {'userId': mockUserId}}));
+    const exampleEvent = {'pathParameters' : {'voteId' : mockId}}
+    const lambdaCallback = (err, data) => {
+      try {
+        expect(data.statusCode).toBe(200);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.deleteOne(exampleEvent, {}, lambdaCallback)
+  });
+
+  test('Test delete vote not found', done => {
+    AWS.DynamoDB.DocumentClient.prototype.delete.mockImplementationOnce((_, callback) => callback(null, {}));
+    const exampleEvent = {'pathParameters' : {'voteId' : mockId}}
+    const lambdaCallback = (err, data) => {
+      try {
+        expect(data.statusCode).toBe(404);
+        done();
+      } catch (error) {
+        done(error);
+      };
+    };
+    votes.deleteOne(exampleEvent, {}, lambdaCallback)
+  });
---
+++ lists-service/api/v1/votes.js
@@ -0,0 +1,76 @@
+'use strict';
+const uuid = require('uuid');
+
+
+const ApiHandlerBase = require('./apiHandlerBase.js')
+
+function Vote(userId, listId, itemId, score){
+  const timestamp = new Date().getTime();
+  this.voteId = uuid.v1();
+  this.userId = userId;
+  this.listId_itemId = list_id + "_" + itemId;
+  this.listId = listId;
+  this.itemId = itemId;
+  this.score = score;
+  this.createdAt = timestamp;
+  this.updatedAt = timestamp;
+};
+
+class VotesApiHandler extends ApiHandlerBase {
+  createOne(event, callback){
+    const requestBody = JSON.parse(event.body);
+    const listId = requestBody.listId;
+    const itemId = requestBody.itemId;
+    const score = requestBody.score;
+    // TODO: validate request
+    console.debug('Validating vote...');
+    const item = new Vote(userId, listId, itemId, score)
+    super.createOne(item, callback)
+  }
+  getAll(event, callback){
+    let limit = this.resultsDefaultLimit;
+    let nextCursor = null;
+    if (event.queryStringParameters){
+      if ('limit' in event.queryStringParameters)
+      {
+        limit = event.queryStringParameters.limit;
+      }
+      if ('nextCursor' in event.queryStringParameters){
+        nextCursor = event.queryStringParameters.nextCursor;
+      }
+    }
+
+    super.getAll(callback, limit=limit, nextCursor=nextCursor)
+  }
+  getOne(event, callback){
+    const key = { 'listId' : event.pathParameters.listId };
+    super.getOne(key, callback)
+  }
+  deleteOne(event, callback){
+    const key = { 'listId' : event.pathParameters.listId };
+    super.deleteOne(key, callback)
+  } 
+}
+
+const apiHandler = new VotesApiHandler(process.env.VOTES_TABLE);
+
+// ------- CREATE ONE ---------
+module.exports.createOne = (event, context, callback) => {
+  apiHandler.createOne(event, callback);
+};
+
+// ------- GET ONE ---------
+module.exports.getOne = (event, context, callback) => {
+  apiHandler.getOne(event, callback);
+};
+
+// ------- GET ALL ---------
+module.exports.getAll = (event, context, callback) => {
+  console.log(event)
+  apiHandler.getAll(event, callback);
+};
+
+// ------- DELETE ONE ---------
+module.exports.deleteOne = (event, context, callback) => {
+  apiHandler.deleteOne(event, callback);
+};
